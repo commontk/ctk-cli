b012d5a86f7d2aa234be9aa1b70d6efbe4e96445
Hans Meine <hans_meine@gmx.net>
2014-11-11 09:14:27 +0100

    add setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name = "ctk-cli",
+    version = "1.0",
+    description = "Python interface for inspecting and running CLI modules (as defined by CommonTK)",
+    license = 'Apache 2.0',
+    keywords = "CTK CLI Slicer host plugin module execution model XML",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2'
+    ],
+    author='Hans Meine',
+    author_email='hans_meine@gmx.net',
+    url='https://github.com/commontk/ctk-cli'
+)
