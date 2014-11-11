a946b6dc22f86bc133642f62441d242018d26396
Hans Meine <hans_meine@gmx.net>
2014-11-11 09:24:08 +0100

    fix setup.py to actually package the code

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "ctk-cli",
     version = "1.0",
+    packages = find_packages(),
     description = "Python interface for inspecting and running CLI modules (as defined by CommonTK)",
     license = 'Apache 2.0',
     keywords = "CTK CLI Slicer host plugin module execution model XML",
