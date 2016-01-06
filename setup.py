d9a78d5b3e7f1b52f6bc05464df6edfc0adae539
Hans Meine <hans_meine@gmx.net>
2016-01-05 23:11:21 -0500

    version bump (1.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ctk-cli",
-    version = "1.0",
+    version = "1.1",
     packages = find_packages(),
     description = "Python interface for inspecting and running CLI modules (as defined by CommonTK)",
     license = 'Apache 2.0',
